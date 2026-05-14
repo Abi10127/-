861848f2317782eb537418ae067f9ca3cbe029b8
Michin <ubaldoabi9@gmail.com>
2026-05-13 22:46:15 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    float valordoble = 3.14;
+    int enterito = 127;
+    
+    printf("El valor flotante mostrado es: %f\n", valordoble);
+    printf("El valor entero mostrado es: %i\n", enterito);
+    return 0;
+}
