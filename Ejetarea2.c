d1aa4e4d6c9c3b7ab954c918e02515e08ae3e078
Michin <ubaldoabi9@gmail.com>
2026-05-13 23:19:10 -0600

    Create Ejetarea2.c

---
+++ Ejetarea2.c
@@ -0,0 +1,19 @@
+//Construir un diagrama de flujo tal que dado el radio de un círculo, calcule
+//e imprima el  ́area y la circunferencia. El  ́area del círculo se calcula como
+//Área = π ∗ radio2 y la circunferencia se calcula como Circunferencia =
+//2 ∗ π ∗ radio.
+
+#include <stdio.h>
+int main()
+{
+  float pi = 3.14;
+  float r;
+  float a = pi*r*r 
+  float c = 2*pi*r
+
+  scanf("Ingresa el valor del radio de la circunferencia: ", &r);
+  printf("El valor del área es: %f\n", a);
+  printf("El valor de la circunferencia es: %f\n", c);
+  return 0;
+}
+  
